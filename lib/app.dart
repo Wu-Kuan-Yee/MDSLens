84eec5e2efc0d6edc938f27d51402430ca893793
gywu <wu_gy@mail.ustc.edu.cn>
2026-07-20 15:03:16 +0800

    Fix Auto theme: use WidgetsBindingObserver + platformDispatcher for system dark mode detection

--- lib/app.dart
+++ lib/app.dart
@@ -4,13 +4,22 @@ import 'models/app_state.dart';
 import 'theme/mdsscope_theme.dart';
 import 'pages/main_page.dart';
 
-class MdsScopeApp extends StatelessWidget {
+class MdsScopeApp extends StatefulWidget {
   const MdsScopeApp({super.key});
+
+  @override
+  State<MdsScopeApp> createState() => _MdsScopeAppState();
+}
+
+class _MdsScopeAppState extends State<MdsScopeApp> with WidgetsBindingObserver {
+  @override void initState() { super.initState(); WidgetsBinding.instance.addObserver(this); }
+  @override void dispose() { WidgetsBinding.instance.removeObserver(this); super.dispose(); }
+  @override void didChangePlatformBrightness() { setState(() {}); }
 
   @override
   Widget build(BuildContext context) {
     final app = context.watch<AppState>();
-    final sysDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+    final sysDark = WidgetsBinding.instance.platformDispatcher.platformBrightness == Brightness.dark;
     final mode = switch (app.themeMode) { 0 => ThemeMode.light, 1 => ThemeMode.dark, _ => sysDark ? ThemeMode.dark : ThemeMode.light };
     return MaterialApp(
       title: 'MdsScope',
